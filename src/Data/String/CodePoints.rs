3196e8abc95148eaf2617ad3fa1f16d4f7beb4f0
0x1 <kevin.francart3@gmail.com>
2026-08-15 19:05:39 +0200

    misc

--- src/Data/String/CodePoints.rs
+++
@@ -1,8 +0,0 @@
-
-pub fn Data_String_CodePoints__unsafeCodePointAt0() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__toCodePointArray() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__fromCodePointArray() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__singleton() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__take() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__countPrefix() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
-pub fn Data_String_CodePoints__codePointAt() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
--- src/Data/String/Unsafe.rs
+++
@@ -1,2 +0,0 @@
-
-pub fn Data_String_Unsafe_charAt() -> crate::UnknownType { crate::UnknownType::new(Record_a { ..Default::default() }) }
