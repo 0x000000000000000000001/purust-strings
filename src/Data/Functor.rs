2dcdd5b973fe2eff133caf1e7e25f2183ea86877
0x1 <kevin.francart3@gmail.com>
2026-08-20 17:19:43 +0200

    misc

--- src/Data/Functor.rs
+++
@@ -1,8 +0,0 @@
-pub fn Data_Functor_arrayMap(mut f: crate::UnknownType, mut arr: crate::UnknownType) -> crate::UnknownType {
-    let mut result = Vec::new();
-    let a = arr.unwrap_array();
-    for i in 0..a.len() {
-        result.push(f.unwrap_func()(a[i].clone()));
-    }
-    crate::mk_array(result)
-}
--- src/Data/Semigroup.rs
+++
@@ -1,9 +0,0 @@
-pub fn Data_Semigroup_concatString(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
-    crate::mk_string(&format!("{}{}", a0.unwrap_string(), a1.unwrap_string()))
-}
-
-pub fn Data_Semigroup_concatArray(mut a0: crate::UnknownType, mut a1: crate::UnknownType) -> crate::UnknownType {
-    let mut vec = a0.unwrap_array().to_vec();
-    vec.extend(a1.unwrap_array().to_vec());
-    crate::mk_array(vec)
-}
