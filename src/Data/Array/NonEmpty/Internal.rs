53a3be45d0e6d615d030dec3be7d1237bbc5953c
0x1 <kevin.francart3@gmail.com>
2026-08-23 13:34:35 +0200

    misc

--- src/Data/Array/NonEmpty/Internal.rs
+++ src/Data/Array/NonEmpty/Internal.rs
@@ -1,26 +1,26 @@
 pub fn Data_Array_NonEmpty_Internal_foldl1Impl() -> crate::UnknownType {
-    crate::Value::Func(std::rc::Rc::new(move |mut f: crate::UnknownType| -> crate::UnknownType {
-            crate::Value::Func(std::rc::Rc::new(move |mut xs: crate::UnknownType| -> crate::UnknownType {
+    crate::Value::Func1(purust_core::Func1::Shared(std::rc::Rc::new(move |mut f: crate::UnknownType| -> crate::UnknownType {
+            crate::Value::Func1(purust_core::Func1::Shared(std::rc::Rc::new(move |mut xs: crate::UnknownType| -> crate::UnknownType {
                     let arr = xs.unwrap_array();
                     let mut acc = arr[0].clone();
                     for i in 1..arr.len() {
                         acc = f.unwrap_func1()(acc).unwrap_func1()(arr[i].clone());
                     }
                     acc
-                }))
-        }))
+                })))
+        })))
 }
 
 pub fn Data_Array_NonEmpty_Internal_foldr1Impl() -> crate::UnknownType {
-    crate::Value::Func(std::rc::Rc::new(move |mut f: crate::UnknownType| -> crate::UnknownType {
-            crate::Value::Func(std::rc::Rc::new(move |mut xs: crate::UnknownType| -> crate::UnknownType {
+    crate::Value::Func1(purust_core::Func1::Shared(std::rc::Rc::new(move |mut f: crate::UnknownType| -> crate::UnknownType {
+            crate::Value::Func1(purust_core::Func1::Shared(std::rc::Rc::new(move |mut xs: crate::UnknownType| -> crate::UnknownType {
                     let arr = xs.unwrap_array();
                     let mut acc = arr[arr.len() - 1].clone();
                     for i in (0..arr.len() - 1).rev() {
                         acc = f.unwrap_func1()(arr[i].clone()).unwrap_func1()(acc);
                     }
                     acc
-                }))
-        }))
+                })))
+        })))
 }
 
