60a17a140b5b1b557357aa002f0a5d4fb19cd10c
0x1 <kevin.francart3@gmail.com>
2026-08-30 20:32:36 +0200

    misc

--- src/Partial/Unsafe.rs
+++ src/Partial/Unsafe.rs
@@ -1,3 +1,3 @@
-pub fn Partial_Unsafe__unsafePartial(mut f: crate::UnknownType) -> crate::UnknownType {
-    f.unwrap_func1()(purust_core::Value::Record_a(perceus_ptr::PerceusPtr::new(purust_core::Record_a { ..Default::default() })))
+pub fn Partial_Unsafe__unsafePartial<a: Clone + 'static>(mut f: purust_core::Func1<crate::UnknownType, a>) -> a {
+    f(purust_core::Value::Record_a(perceus_ptr::PerceusPtr::new(purust_core::Record_a { ..Default::default() })))
 }
